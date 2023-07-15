be94b3877529e3339795337470e9a0fffb4dddc7
yuanjiechen <931339781@qq.com>
2023-07-15 11:07:26 +0000

    fix onnx input device

--- export_controlnet_onnx.py
+++ export_controlnet_onnx.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     # inputs = (torch.randn((1, 4, 32, 48)).cuda(), torch.randn((1, 3, 256, 384)).cuda(), torch.randn((1,)).cuda(), torch.randn((1, 77, 768)).cuda())
     with open("./controlnet.pkl", "rb") as f:
         inputs = pickle.load(f)
+    
+    cuda_inputs = []
     for inp in inputs:
-        inp.cuda()
-    torch.onnx.export(hk.model.control_model.eval(), inputs, "./controlnet.onnx", opset_version=17, do_constant_folding=True)
+        cuda_inputs.append( inp.cuda())
+    torch.onnx.export(hk.model.control_model.eval(), cuda_inputs, "./controlnet.onnx", opset_version=17, do_constant_folding=True)
     
