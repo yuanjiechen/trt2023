41fb0a7a34860d2d300f3a7745178d3343608a99
yuanjiechen <931339781@qq.com>
2023-07-16 07:50:29 +0000

    engine model path

--- preprocess.sh
+++ preprocess.sh
@@ -2,7 +2,7 @@ echo "preprocess"
 mkdir onnxs
 /usr/bin/python3 export_controlnet_onnx.py
 cd onnxs
-trtexec --onnx=./controlnet_full.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_fp16.engine --infStreams=4
-mv controlnet_fp16.engine ../
+trtexec --onnx=./controlnet_full.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_full_fp16.engine --infStreams=4
+mv controlnet_full_fp16.engine ../
 cd ..
 # trtexec --onnx=./controlnet.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --saveEngine=./controlnet.engine --infStreams=4
