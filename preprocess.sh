9f6df090cf81a603e7b90f2b5ba700e32676a8c9
yuanjiechen <931339781@qq.com>
2023-07-18 13:40:22 +0000

    export onnx

--- preprocess.sh
+++ preprocess.sh
@@ -1,6 +1,6 @@
 echo "preprocess"
 mkdir onnxs
-# /usr/bin/python3 export_controlnet_onnx.py
+/usr/bin/python3 export_controlnet_onnx.py
 cd onnxs
 trtexec --onnx=./controlnet_full.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_full_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
 trtexec --onnx=./controlnet_vae.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_vae_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
