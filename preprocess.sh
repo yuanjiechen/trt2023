b5330b1357d3bbdf9febe9731e32b9aabd0547c6
yuanjiechen <931339781@qq.com>
2023-07-19 10:27:52 +0000

    preprocess return

--- preprocess.sh
+++ preprocess.sh
@@ -2,12 +2,12 @@ echo "preprocess"
 mkdir onnxs
 /usr/bin/python3 export_controlnet_onnx.py
 cd onnxs
-# trtexec --onnx=./controlnet_full.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_full_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
-# trtexec --onnx=./controlnet_vae.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_vae_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
-trtexec --onnx=./controlnet_with_vae.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_with_vae_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
+trtexec --onnx=./controlnet_full.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_full_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
+trtexec --onnx=./controlnet_vae.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_vae_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
+# trtexec --onnx=./controlnet_with_vae.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --fp16 --saveEngine=./controlnet_with_vae_fp16.engine --infStreams=4 --maxAuxStreams=10 --useCudaGraph
 # /home/player/.local/bin/polygraphy
-# mv controlnet_full_fp16.engine ../
-# mv controlnet_vae_fp16.engine ../
-mv controlnet_with_vae_fp16.engine ../
+mv controlnet_full_fp16.engine ../
+mv controlnet_vae_fp16.engine ../
+# mv controlnet_with_vae_fp16.engine ../
 cd ..
 # trtexec --onnx=./controlnet.onnx --exportProfile=./profile.json --exportLayerInfo=./layerinfo.json --profilingVerbosity=detailed --workspace=16384 --saveEngine=./controlnet.engine --infStreams=4
